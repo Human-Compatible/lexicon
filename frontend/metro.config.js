5aaf6e72e7244adf4891173ee4f8b5cdcb032479
SimonBiggs <me@simonbiggs.net>
2023-07-04 13:59:45 +1000

    able to run a static site export

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -12,5 +12,13 @@ defaultConfig.transformer = {
   babelTransformerPath: require.resolve('react-native-svg-transformer'),
 };
 defaultConfig.resolver.assetExts = assetExts.filter((ext) => ext !== 'svg');
+
+// https://github.com/facebook/metro/issues/535#issuecomment-1016709406
+defaultConfig.resolver.sourceExts = process.env.RN_SRC_EXT
+    ? [...process.env.RN_SRC_EXT.split(',').concat(defaultConfig.resolver.sourceExts), 'cjs'] // <-- cjs added here
+    : [...defaultConfig.resolver.sourceExts, 'cjs'] // <-- cjs added here
+
 defaultConfig.resolver.sourceExts.push('svg');
+
+
 module.exports = defaultConfig;
